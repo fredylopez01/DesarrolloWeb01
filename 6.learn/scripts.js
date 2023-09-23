80780b66a35dfb29a486d784fea14832e9a4e8cf
Fredy Lopez <133995369+fredylopez01@users.noreply.github.com>
2023-09-23 18:08:53 -0500

    Ready

--- 6.learn/scripts.js
+++ 6.learn/scripts.js
@@ -1,5 +1,21 @@
-funtion saludo(){
-
-	alert("Este se ejcuta desde un archivo independiente");
-	
+function ejecuta(){
+	//document.getElementsByTagName("p")[2].onclick=saludo;
+	//document.getElementById("second").onclick=saludo;
+	for (let i = 0; i < document.getElementsByTagName("p").length; i++) {
+		document.getElementsByTagName("p")[i].onclick=saludo;
+	}
 }
+
+function selector(){
+	document.querySelector("p").onclick=saludo;
+}
+
+function variosSelector(){
+	var elemento= document.querySelectorAll("p", "span")[0].onclick=saludo;
+}
+
+function saludo(){
+	alert("Este se ejcuta desde un archivo independiente");
+}
+
+window:onload=variosSelector;
